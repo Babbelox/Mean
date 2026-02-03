eb0c7de3c742a4da2d675d3d5cd8d7b41eff1a56
Babbelox <gabriele.garzillo@mail.polimi.it>
2026-02-03 23:02:38 +0100

    ✨added add_token function

--- Compiler/Compiling.h
+++ Compiler/Compiling.h
@@ -1,8 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 
-int tokenizer(char buff){
+//For now i include the struct, but in the future I'll make it work with void pointers
+typedef struct Token {
+    int tokenID;
+    char lex[256];
+    struct Token* next;
+} Token;
+
+
+void comparer(char* line) {
     
 }
 
-void comparer(int token) {
-    
+Token* add_token(Token* head, int tokenID){
+    Token* new_token, *new_head;
+
+    new_token = (Token*)malloc(sizeof(Token));
+    new_token->tokenID = tokenID;
+    for(new_head = head; new_head->next != NULL; new_head = new_head->next);
+    new_head->next = new_token;
+
+    if(tokenID==-1){
+        comparer(new_token->lex);
+    }
+
+    else
+        free(new_token->lex);
+
+    return head;
+
 }
